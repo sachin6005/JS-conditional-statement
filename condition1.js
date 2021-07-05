a0a0d3743a83d0a1af8ad860b462730bb3f638ce
sachin kumar <merugune.sachin123@gmail.com>
2021-07-06 00:29:06 +0530

    problems done on if-else conditional statement

---
+++ condition1.js
@@ -0,0 +1,20 @@
+var length = 200
+var breadth = 200
+
+if (length==breadth){
+  console.log('true')
+}
+  
+else {
+  console.log('false')
+}
+
+var length1 = 300
+var breadth1 = 200
+
+if (length1=='breadth1') {
+   console.log('true') 
+}
+else {
+  console.log('false')
+}
---
+++ condition2.js
@@ -0,0 +1,45 @@
+var num1 = 5
+var num2 = 8
+var num3 = 20
+
+
+if (num1>num2) {
+   console.log('num1 is greater') 
+}
+else if (num2>num3) {
+  console.log('num2 is greater')
+}
+else  {
+  console.log('num3 is greater')
+}
+
+
+var num1 = 25
+var num2 = 8
+var num3 = 20
+
+
+if (num1>num2) {
+   console.log('num1 is greater') 
+}
+else if (num2>num3) {
+  console.log('num2 is greater')
+}
+else  {
+  console.log('num3 is greater')
+}
+
+var num1 = 25
+var num2 = 28
+var num3 = 20
+
+
+if (num1>num2) {
+   console.log('num1 is greater') 
+}
+else if (num2>num3) {
+  console.log('num2 is greater')
+}
+else  {
+  console.log('num3 is greater')
+}
